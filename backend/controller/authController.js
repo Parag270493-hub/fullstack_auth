ce9b396583eeb7275bfe5e672df32457df30186c
Parag <coolttparag1@gmail.com>
2025-06-18 12:57:18 +0530

    added backend code
    added route, controller, middleware, models, server.js,

---
+++ backend/controller/authController.js
@@ -0,0 +1,36 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, mobile, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, mobile, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered' });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: 'Invalid email' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: 'Invalid password' });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getUsers = async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+};
